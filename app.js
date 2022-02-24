faf73986058b3ccc00de7d7a5f5a94db279ceffc
Mahtab-Jeyhani <mahtabjeihani@gmail.com>
2022-02-24 19:40:23 +0330

    changed icons

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#Temperature");
@@ -15,6 +16,8 @@ function displayTemperature(response) {
     let pressureElement = document.querySelector("#pressure");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -22,7 +25,9 @@ function displayTemperature(response) {
     pressureElement.innerHTML = Math.round(response.data.main.pressure);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `pics/${response.data.weather[0].description}.png`);
 }
 let ApiKey = "2bb25f83d9730c68e1f5d645832d1509";
-let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${ApiKey}&units=metric`;
+let city = "New York"
+let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}&units=metric`;
 axios.get(ApiUrl).then(displayTemperature);
